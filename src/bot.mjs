237c7050b30876ef7e1c011296964d70188b7749
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-05-26 02:34:12 +0500

    Redis demo

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,6 +1,23 @@
-import {Bot} from "grammy";
+import {Bot, session} from "grammy";
+import {kv as instance} from "@vercel/kv";
+import {RedisAdapter} from "@grammyjs/storage-redis";
+
+instance.opts.automaticDeserialization = false;
+
+const storage = new RedisAdapter({instance, ttl: 10});
 
 export const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.use(
+    session({
+        initial: () => ({counter: 0}),
+        storage,
+    }),
+);
+
+bot.command('stats', ctx => ctx.reply(`Already got ${ctx.session.counter} photos!`));
+
+bot.on(':photo', ctx => ctx.session.counter++);
 
 bot.on("message:text", async ctx => ctx.reply(ctx.msg.text));
 
