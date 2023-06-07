137dabb0d09186d006eaaf1df9cb5a4dba4b69dc
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-06-07 15:42:15 +0500

    MongoDB demo

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,6 +1,23 @@
-import {Bot} from "grammy";
+import {Bot, session} from "grammy";
+import {MongoClient} from "mongodb";
+import {MongoDBAdapter} from "@grammyjs/storage-mongodb";
 
-export const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
+const {TELEGRAM_BOT_TOKEN, MONGODB_URI} = process.env;
+
+export const bot = new Bot(TELEGRAM_BOT_TOKEN);
+export const client = await MongoClient.connect(MONGODB_URI);
+export const collection = client.db("grammY").collection("users");
+
+bot.use(
+    session({
+        initial: () => ({counter: 0}),
+        storage: new MongoDBAdapter({collection}),
+    })
+);
+
+bot.command("stats", ctx => ctx.reply(`Already got ${ctx.session.counter} photos!`));
+
+bot.on(":photo", ctx => ctx.session.counter++);
 
 bot.on("message:text", async ctx => ctx.reply(ctx.msg.text));
 
